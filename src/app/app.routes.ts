9e055bc2ec540ae4aeeddd4beaf2b878dc68d54d
Kostas Veloutsos <kwstasvel96@gmail.com>
2026-07-04 14:38:52 +0300

    insert customer form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { CustomerForm } from './components/customer/customer-form/customer-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'customers', component: CustomerForm }
+];
---
+++ src/app/models/customer.model.ts
@@ -0,0 +1,26 @@
+export interface CustomerInsert {
+  firstname: string;
+  lastname: string;
+  email?: string;
+  phone: string;
+  address?: string;
+  postalCode?: string;
+  vat: string;
+  companyName?: string;
+  regionId: number;
+}
+
+export interface Customer {
+  id: number;
+  uuid: string;
+  firstname: string;
+  lastname: string;
+  email?: string | null;
+  phone: string;
+  address: string;
+  postalCode: string;
+  vat?: string | null;
+  companyName?: string | null;
+  balance: number;
+  regionId: number;
+}
